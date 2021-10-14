d293c7918fb1551c1f6e9729756666516b33c3e3
abdulahi roble <abdulahi_2610@hotmail.com>
2021-10-14 12:08:04 +0200

    create server

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    // this also works:
+    // res.sendFile("index.html", { root: __dirname });
+    res.sendFile(__dirname + "/public/frontpage/index.html");
+});
+
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT, (error) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("Server is running on port", server.address().port);
+});
